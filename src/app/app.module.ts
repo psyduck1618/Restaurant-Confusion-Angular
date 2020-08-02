6f4cd38d781c8d79ae363a102ba638c6ca2de1df
Amaan Khan <amaankhan.dev@gmail.com>
2020-08-02 18:28:10 +0530

    Dish1

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import{MatListModule} from '@angular/material/list';
+import { MatSliderModule } from '@angular/material/slider';
+import { AppComponent } from './app.component';
+
+import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatListModule,
+    MatSliderModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
