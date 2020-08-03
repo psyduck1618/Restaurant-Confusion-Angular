88f7d6d88974d53a77a09ac5c8967b58f2fcd81e
Amaan Khan <amaankhan.dev@gmail.com>
2020-08-03 18:45:26 +0530

    assignment1.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import {FlexLayoutModule} from '@angular/flex-layout';
 import{MatListModule} from '@angular/material/list';
+import { MatGridListModule} from '@angular/material/grid-list';
 import { MatSliderModule } from '@angular/material/slider';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import{ MatCardModule } from '@angular/material/card';
+import{ MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
+
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule,
@@ -22,6 +28,9 @@ import { MenuComponent } from './menu/menu.component';
     MatToolbarModule,
     MatListModule,
     MatSliderModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
